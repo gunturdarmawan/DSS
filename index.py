88874e837ae03230b675ce76514b820b8750f179
guntur darmawan <19523166@students.uii.ac.id>
2023-04-20 01:03:05 +0700

    try 2

--- index.py
+++ index.py
@@ -1,10 +1,17 @@
-from flask import Flask
+import datetime
+import os
+import pickle
+
+import pandas as pd
+from flask import Flask, render_template, request
+from sklearn.preprocessing import minmax_scale
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    return render_template("index.html")
 
 @app.route('/about')
 def about():
